af0db9eda6f3a58893ef9006130ec4e65af5f9dd
Daniel Avila (sparky) <sparkytwobillion@gmail.com>
2015-09-20 12:45:38 -0500

    added for conversion from script to unittest

---
+++ superlist/functional_test2.py
@@ -0,0 +1,31 @@
+__author__ = 'sparky'
+import unittest
+from selenium import webdriver
+
+class HomePage(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_home_page(self):
+        """Does To-Do show up in the browser's title for the home page?"""
+        expected = 'To-Do'
+        self.browser.get('http://localhost:8000')
+        result = self.browser.title
+        self.assertIn(expected, result)
+
+
+#    def test_<reasonable_name>(self):
+        # """Human readable description of the test goes here.
+
+        # The first line should be the description. It can be multi line.
+        # I like to make the first line a question
+        # """
+         #Define expected values
+
+         #Run the code under test to get the actual value
+
+         #Compair the expected value with the actual value
